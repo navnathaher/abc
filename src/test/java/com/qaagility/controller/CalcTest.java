1106b3660a66cfa2a8004e1f13b993afcb9012e9
navnathaher <navnath.aher@amdocs.com>
2019-03-14 13:00:08 +0530

    method creation

--- src/test/java/com/qaagility/controller/CalcTest.java
+++ src/test/java/com/qaagility/controller/CalcTest.java
@@ -5,12 +5,13 @@ import static org.junit.Assert.assertEquals;
 import com.qaagility.javaee.*;
 
     @Test
-    public void testCal() {
+    public void testCal(){
 
 Calcmul testcalmul = new Calcmul();
 Calculator testcaladd= new Calculator();
 
 		assertEquals(testcalmul.mul(),18);
 		assertEquals(testcaladd.add(),9);
+
 } 
 
